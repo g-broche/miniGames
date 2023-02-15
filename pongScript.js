33f3d811c1e8c8e22a7624f2836c26c132f0c5f2
greg <gregory.broche@yahoo.fr>
2023-02-15 11:15:24 +0100

    fixed issue with game pausing and made code cleaner overall

--- pongScript.js
+++ pongScript.js
@@ -1,29 +1,39 @@
 "use strict"
 
+/* ***** global objects and variables ***** */
 
-const increment = 2;
+// area on which the pong game is displayed
+const GameWindow = {
+    elem: document.getElementById("pongGameWindow"),
+    width: document.getElementById("pongGameWindow").offsetWidth,
+    height: document.getElementById("pongGameWindow").offsetHeight,
+};
 
+// vertical movement of bars
+const barMoveIncrementPercent = 1;
+let barMovementValue = GameWindow.height / 100 * barMoveIncrementPercent;
+
+//keymapping (CURRENTLY NOT IMPLEMENTED)
 const keyPlayer1Up = "KeyW";
 const keyPlayer1Down = "KeyS";
 const keyPlayer2Up = "ArrowUp";
 const keyPlayer2Down = "ArrowDown";
 const pause = "Space";
 
+//bar default parameters
 const defaultBarHeight = 100;
 const minBarHeight = 20;
 const defaultBarWidth = 20;
 
+//game booleans
 let isInitialTurn = true;
 let gameEnded = false;
-
-let requestAnimationId;
 let isPaused = false;
 
-const GameWindow = {
-    elem: document.getElementById("pongGameWindow"),
-    width: document.getElementById("pongGameWindow").offsetWidth,
-    height: document.getElementById("pongGameWindow").offsetHeight,
-};
+//id of current animation frame
+let requestAnimationId;
+
+/* ***** Creating object score handling the score of each player and the related methods ***** */
 
 const Score = {
     player1Field: document.getElementById("scorePlayer1"),
@@ -32,6 +42,7 @@ const Score = {
     player2Points: 0,
 };
 
+//display the current score in the corresponding element
 Score.display = function () {
     this.player1Field.textContent = this.player1Points;
     this.player2Field.textContent = this.player2Points;
@@ -42,13 +53,18 @@ Score.update = function (winnerBool) {
     (winnerBool ? this.player1Points++ : this.player2Points++);
     this.display();
 };
+
+//reset score (CURRENTLY NOT IMPLEMENTED)
 Score.reset = function () {
     this.player1Points = 0;
     this.player2Points = 0;
     this.display();
 };
 
-const bar = {
+/* ***** Creating both bars ***** */
+
+//object Bar containing the methods which will be inherited by the player bars on each side
+const Bar = {
     setInitialValues({ elem, width, height, defaultPositionY, topPosition }) {
         this.elem = elem;
         this.width = width;
@@ -56,12 +72,14 @@ const bar = {
         this.defaultPositionY = defaultPositionY;
         this.topPosition = topPosition;
     },
+
     resetPosition() {
         this.defaultPositionY = (GameWindow.height / 2) - (this.height / 2)
         this.topPosition = this.defaultPositionY;
         this.elem.style.top = this.defaultPositionY + "px";
     },
-    fullReset() {
+
+    fullReset() { //Not implemented currently
         this.height = defaultBarHeight;
         this.width = defaultBarWidth;
         this.defaultPositionY = (GameWindow.height / 2) - (defaultBarHeight / 2);
@@ -73,13 +91,54 @@ const bar = {
         this.height = (this.height + (this.height / 100 * value) < defaultBarHeight ? this.height + (this.height / 100 * value) : defaultBarHeight);
         this.elem.style.height = this.height + "px";
     },
+
     reduceHeight(value) {
         this.height = (this.height - (this.height / 100 * value) > minBarHeight ? this.height - (this.height / 100 * value) : minBarHeight);
         this.elem.style.height = this.height + "px";
-    }
+    },
+
+    moveUp() {
+        if (this.canMoveUp()) {
+            this.move(-barMovementValue);
+        } else {
+            this.moveMaxHeight();
+        }
+    },
+
+    moveDown() {
+        if (this.canMoveDown()) {
+            this.move(barMovementValue);
+        } else {
+            this.moveMinHeight();
+        }
+    },
+
+    move(value) {
+        this.topPosition += Number(value);
+        this.elem.style.top = this.topPosition + "px";
+    },
+
+    canMoveUp() {
+        return (this.topPosition - barMovementValue >= 0 ? true : false);
+    },
+
+    canMoveDown() {
+        return (this.topPosition + this.height + barMovementValue <= GameWindow.height ? true : false);
+    },
+
+    moveMaxHeight() {
+        this.topPosition = 0;
+        this.elem.style.top = this.topPosition + "px";
+    },
+
+    moveMinHeight() {
+        this.topPosition = GameWindow.height - this.height;
+        this.elem.style.top = this.topPosition + "px";
+    },
 };
 
-const LeftBar = Object.create(bar);
+//creating the left and right bar using the methods of Bar object
+const LeftBar = Object.create(Bar);
 LeftBar.setInitialValues({
     elem: document.getElementById("leftBar"),
     width: defaultBarWidth,
@@ -88,7 +147,7 @@ LeftBar.setInitialValues({
     topPosition: (GameWindow.height / 2) - (defaultBarHeight / 2),
 });
 
-const RightBar = Object.create(bar);
+const RightBar = Object.create(Bar);
 RightBar.setInitialValues({
     elem: document.getElementById("rightBar"),
     width: defaultBarWidth,
@@ -97,6 +156,8 @@ RightBar.setInitialValues({
     topPosition: (GameWindow.height / 2) - (defaultBarHeight / 2),
 });
 
+
+/* ***** Ball object and its related methods ***** */
 const Ball = {
     elem: document.querySelector("i"),
     defaultPositionY: (GameWindow.height / 2),
@@ -112,86 +173,55 @@ const Ball = {
 Ball.reset = function () {
     this.centerX = this.defaultPositionX
     this.centerY = this.defaultPositionY
-}
+};
 Ball.toggleDirectionX = function () {
     this.directionX = (this.directionX ? false : true);
-}
+};
 
 Ball.toggleDirectionY = function () {
     this.directionY = (this.directionY ? false : true);
+};
+
+/* ***** HANDLING OF PLAYER INPUTS ***** */
+
+//object containing the player controls, their states and the events to trigger if their pressed state is true
+const actionLog = {
+    "KeyW": { pressed: false, func: () => { LeftBar.moveUp() } },
+    "KeyS": { pressed: false, func: () => { LeftBar.moveDown() } },
+    "ArrowUp": { pressed: false, func: () => { RightBar.moveUp() } },
+    "ArrowDown": { pressed: false, func: () => { RightBar.moveDown() } },
+    "Space": { pressed: false, func: pauseGame },
 }
 
-
-/* ***** BAR MOVEMENT ***** */
-
+//event listener for keydown, will first check if the game has to be resume and if not will update the action to execute at each frame cycle based on key triggers
 document.addEventListener("keydown", function (event) {
-    let barMovementValue = GameWindow.height / 100 * increment;
-
-
-    switch (event.code) {
-
-        case keyPlayer1Up:
-            if (!isPaused) { moveUp(LeftBar, -barMovementValue) };
-            break;
-        case keyPlayer1Down:
-            if (!isPaused) { moveDown(LeftBar, barMovementValue) };
-            break;
-        case keyPlayer2Up:
-            if (!isPaused) { moveUp(RightBar, -barMovementValue) };
-            break;
-        case keyPlayer2Down:
-            if (!isPaused) { moveDown(RightBar, barMovementValue) };
-            break;
-        case pause:
-            pauseGame();
-            break;
-        default:
-            break;
+    if (event.code == "Space" && isPaused == true) {
+        resumeGame()
+    }
+    else if (actionLog[event.code]) {
+        actionLog[event.code].pressed = true;
     }
 });
 
-function moveUp(bar, value) {
-    if (canBarMoveUp(bar, value)) {
-        moveBar(bar, value);
-    } else {
-        moveMaxHeight(bar)
+//event listener for keyup, will put the boolean value for the related key to false into the actionLog object to stop firing the corresponding function at each frame cycle 
+document.addEventListener("keyup", function (event) {
+    if (actionLog[event.code]) {
+        actionLog[event.code].pressed = false;
     }
-}
+});
 
-function moveDown(bar, value) {
-    if (canBarMoveDown(bar, value)) {
-        moveBar(bar, value)
-    } else {
-        moveMinHeight(bar)
-    }
-}
-
-function moveBar(bar, value) {
-
-    bar.topPosition += Number(value);
-    bar.elem.style.top = bar.topPosition + "px";
-}
-
-function moveMaxHeight(bar) {
-    bar.topPosition = 0;
-    bar.elem.style.top = bar.topPosition + "px";
-}
-function moveMinHeight(bar) {
-    bar.topPosition = GameWindow.height - bar.height;
-    bar.elem.style.top = bar.topPosition + "px";
-}
-
-function canBarMoveUp(bar, value) {
-
-    return (bar.topPosition + value >= 0 ? true : false);
-}
-function canBarMoveDown(bar, value) {
-    return (bar.topPosition + bar.height + value <= GameWindow.height ? true : false);
-}
-
-/* ***** BALL MOVEMENT ***** */
+//goes through all the keys stored into the actionLog and if a key.pressed state is true will then fire its associated function
+function executeInputs() {
+    Object.keys(actionLog).forEach(key => {
+        actionLog[key].pressed && actionLog[key].func();
+    })
+};
 
 
+
+/* ***** Functions related to Ball movement ***** */
+
+//updates coords properties of the ball
 function changeBallCoords(moveValue) {
     (Ball.directionX ? (Ball.centerX += moveValue) : (Ball.centerX -= moveValue));
     if (isInitialTurn == false) {
@@ -199,12 +229,13 @@ function changeBallCoords(moveValue) {
     }
 }
 
+//moves the ball around based on its coordinate variables
 function displayBallMovement() {
     Ball.elem.style.top = (Ball.centerY - Ball.radius) + "px";
     Ball.elem.style.left = (Ball.centerX - Ball.radius) + "px";
 }
 
-
+//checks if ball will hit the bar on the right side at next movemement, returns true if yes and false otherwise
 function willBallHitRightBar(moveValueX, moveValueY) {
     if ((Ball.centerX + Ball.radius + moveValueX >= RightBar.elem.offsetLeft) && (Ball.centerX + Ball.radius + moveValueX <= RightBar.elem.offsetLeft + RightBar.width) && (Ball.centerY + Ball.radius + moveValueY >= RightBar.topPosition) && (Ball.centerY - Ball.radius + moveValueY <= RightBar.topPosition + RightBar.height)) {
         return true;
@@ -212,6 +243,7 @@ function willBallHitRightBar(moveValueX, moveValueY) {
         return false;
     }
 }
+//checks if ball will hit the bar on the left side at next movemement, returns true if yes and false otherwise
 function willBallHitLeftBar(moveValueX, moveValueY) {
     if ((Ball.centerX - Ball.radius + moveValueX <= LeftBar.elem.offsetLeft + LeftBar.width) && (Ball.centerX - Ball.radius + moveValueX >= LeftBar.elem.offsetLeft) && (Ball.centerY + Ball.radius + moveValueY >= LeftBar.topPosition) && (Ball.centerY - Ball.radius + moveValueY <= LeftBar.topPosition + LeftBar.height)) {
         return true;
@@ -220,10 +252,12 @@ function willBallHitLeftBar(moveValueX, moveValueY) {
     }
 }
 
+//checks if either of the bars will be hit by the ball on next move, returns true if that's the case and false otherwise
 function willBallHitAnyBar(moveValueX, moveValueY) {
     return willBallHitRightBar(moveValueX, moveValueY) || willBallHitLeftBar(moveValueX, moveValueY);
 }
 
+//changes the ball direction depending on which half of the bar's height is hit by the ball
 function redirectFromBar(bar) {
     if (isInitialTurn) {
         isInitialTurn = false;
@@ -232,6 +266,7 @@ function redirectFromBar(bar) {
     Ball.toggleDirectionX();
 }
 
+//get the ball into the point of direct contact to the nearest bar (to smooth redirection), returns the distance that was necessary to close in
 function closeDistanceToBar() {
     let distanceToReach = 0;
     if (Ball.directionX) {
@@ -241,14 +276,16 @@ function closeDistanceToBar() {
     }
     changeBallCoords(distanceToReach)
     displayBallMovement();
-    distanceToReach;
+    return distanceToReach;
 
 }
 
+//checks if the ball will hit either the top or bottom part of gameWindow returns true if yes and no otherwise
 function willBallHitAnyWall(moveValueY) {
     return ((Ball.centerY + Ball.radius + moveValueY >= GameWindow.height) || (Ball.centerY - Ball.radius + moveValueY <= 0));
 }
 
+//get the ball into the point of direct contact to the nearest wall (to smooth redirection), returns the distance that was necessary to close in
 function closeDistanceToWall() {
     let distanceToReach = 0;
     if (Ball.directionY) {
@@ -262,51 +299,49 @@ function closeDistanceToWall() {
 
 }
 
+//function handling everything related to ball movement and calculations
 function moveBall() {
-    //check if during the next move the ball will hit either a bar or a wall
-    let ballTravel = Ball.velocity;
-    let i = 10;
+    let ballTravel = Ball.velocity; //stores the amout of movement necessary to travel during the frame animation cycle
 
-    while (ballTravel > 0 && i >= 0) {
-        let moveX = (Ball.directionX ? ballTravel : -ballTravel);
+    //for as long as there is movement to do :
+    while (ballTravel > 0) {
+        let moveX = (Ball.directionX ? ballTravel : -ballTravel);   //sets value of movement to do based of movement direction
         let moveY = (Ball.directionY ? ballTravel : -ballTravel);
-        if (willBallHitAnyBar(moveX, moveY)) {
-            ballTravel -= closeDistanceToBar();
-            (Ball.directionX ? redirectFromBar(RightBar) : redirectFromBar(LeftBar))
-        } else if (willBallHitAnyWall(moveY) && (Ball.centerY - Ball.radius != 0)) {
-            ballTravel -= closeDistanceToWall();
-            Ball.toggleDirectionY();
+        if (willBallHitAnyBar(moveX, moveY)) {                                          //if during this move the ball will hit a bar
+            ballTravel -= closeDistanceToBar();                                         //close the distance to the bar and reduce the movement left to do during this animation cycle
+            (Ball.directionX ? redirectFromBar(RightBar) : redirectFromBar(LeftBar))    //apply change of direction according to side hit and which part of the bar was hit
+        } else if (willBallHitAnyWall(moveY) && (Ball.centerY - Ball.radius != 0)) {        //else, check if ball will hit any wall during this move
+            ballTravel -= closeDistanceToWall();                                            //close the distance to the wall and reduce the movement left to do during this animation cycle
+            Ball.toggleDirectionY();                                                        //change vertical movement direction
         } else {
-            changeBallCoords(ballTravel);
+            changeBallCoords(ballTravel);   //if neither previous case happened in this loop, use the totality of allocated movement to move the ball
             displayBallMovement();
             ballTravel = 0;
         }
     }
 }
 
-function animateBall() {
-    moveBall()
-    hasGameEnded();
-    if (gameEnded) {
-        Score.update(Ball.directionX);
-        increaseDifficulty(Ball.directionX);
-        startRound();
-    } else {
-        requestAnimationId = window.requestAnimationFrame(animateBall);
-    }
-}
+/* ***** function related to game pacing and winning event ***** */
 
+//pauses the game by cancelling animationframe and turns the pressed state of the pause button to false
 function pauseGame() {
-
-    if (isPaused) {
-        animateBall();
-        isPaused = false;
-    } else {
+    if (!isPaused) {        // if game wasn't paused then
+        isPaused = true;    // isPaused becomes true and the animation frame is cancelled based on id
         window.cancelAnimationFrame(requestAnimationId);
-        isPaused = true;
+        actionLog["Space"].pressed = false;
     }
 }
 
+//resumes game by calling the function running the entire game logic and turns the pressed state of the pause button to false
+function resumeGame() {
+    if (isPaused) {         // if game was paused then
+        isPaused = false;   // isPaused boolean becomes false and animateBall starts
+        actionLog["Space"].pressed = false;
+        animateBall();
+    }
+}
+
+//reset position of elements and state of game logic booleans on round restart
 function startRound() {
     LeftBar.resetPosition();
     RightBar.resetPosition();
@@ -317,29 +352,36 @@ function startRound() {
     animateBall();
 }
 
-
+//win condition
 function hasGameEnded() {
     gameEnded = (((Ball.centerX + Ball.radius <= 0) || (Ball.centerX - Ball.radius >= GameWindow.width)) ? true : false);
 }
 
-//function changeBarDefaultPosition
-
-
+//change bar properties for each player depending of if winning or losing based on a boolean arcgument (will use ballDirectionX to deternine who won)
 function increaseDifficulty(player2Won) {
     if (player2Won) {
         LeftBar.reduceHeight(20);
         RightBar.increaseHeight(10);
-        LeftBar.resetPosition();
-        RightBar.resetPosition();
+
     } else {
         RightBar.reduceHeight(20);
         LeftBar.increaseHeight(10);
-        LeftBar.resetPosition();
-        RightBar.resetPosition();
     }
 }
 
-
+//function that will run the whole game logic, will do a recursion at the end by calling itself into a requestAnimationFrame thus causing a frame cycle
+function animateBall() {
+    executeInputs();
+    moveBall();
+    hasGameEnded();
+    if (gameEnded) {
+        Score.update(Ball.directionX);
+        increaseDifficulty(Ball.directionX);
+        startRound();
+    } else if (!isPaused) {
+        requestAnimationId = window.requestAnimationFrame(animateBall);
+    }
+}
 
 /* ***** INITIALIZATION ***** */
 Score.reset()
